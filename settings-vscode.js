93ea7c1e2805dad18c1d157f9e11f41cf4e4b41c
Caio Cezar Guedes de Queiroz <caio.cezar.dequeiroz@gmail.com>
2023-05-23 09:23:13 -0300

    Fix/lint node (#2)
    * refactor: refactored readme.md to vscode workspace settings
    
    * refactor: refactored settings-vscode to create vscode workspace

--- settings-vscode.js
+++ settings-vscode.js
@@ -1,4 +1,6 @@
-const settingsVscode = {
+const { existsSync, writeFileSync, mkdirSync } = require('node:fs');
+const _PATH_FOLDER = './.vscode/settings.json';
+const _DEFAULT_CONFIGS_WORKSPACE = {
   'files.autoSave': 'afterDelay',
   'editor.formatOnSave': true,
   'editor.codeActionsOnSave': {
@@ -10,4 +12,36 @@ const settingsVscode = {
   'workbench.settings.editor': 'json',
 };
 
-module.exports = JSON.stringify(settingsVscode);
+(() => {
+  let settings = {};
+  let hasExistFile = existsSync(_PATH_FOLDER) ?? false;
+  //check has file settings.json inside .vscode folder
+  if (existsSync(_PATH_FOLDER)) settings = require(_PATH_FOLDER);
+  //include non-existing properties in the file
+  for (const key in _DEFAULT_CONFIGS_WORKSPACE) {
+    if (settings.hasOwnProperty(key)) {
+      if (typeof _DEFAULT_CONFIGS_WORKSPACE[key] !== 'string') {
+        for (const keyChild in _DEFAULT_CONFIGS_WORKSPACE[key]) {
+          if (!settings[key].hasOwnProperty(keyChild)) {
+            settings[key][keyChild] = _DEFAULT_CONFIGS_WORKSPACE[key][keyChild];
+          }
+        }
+      }
+    } else {
+      settings[key] = _DEFAULT_CONFIGS_WORKSPACE[key];
+    }
+  }
+  try {
+    const settingsJSON = JSON.stringify(settings);
+    //
+    if (!hasExistFile) {
+      if (!existsSync(_PATH_FOLDER.replace('/settings.json', ''))) {
+        mkdirSync(_PATH_FOLDER.replace('/settings.json', ''));
+      }
+    }
+    writeFileSync(_PATH_FOLDER, settingsJSON);
+    console.log('Arquivo settings.json criado com sucesso.');
+  } catch (err) {
+    console.error('Erro ao criar o arquivo settings.json:', err);
+  }
+})();
