d8651cf4a30de9cba38404a1a97fd8f45b0bb197
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2023-05-23 12:14:12 -0300

    refactor: change local to import file

---
+++ settingsGenerator/vscode/index.js
@@ -0,0 +1,49 @@
+/* eslint-disable no-console */
+const { existsSync, writeFileSync, mkdirSync } = require('node:fs');
+const _PATH_FOLDER = `${process.cwd()}/.vscode/settings.json`;
+const _DEFAULT_CONFIGS_WORKSPACE = {
+  'files.autoSave': 'afterDelay',
+  'editor.formatOnSave': true,
+  'editor.codeActionsOnSave': {
+    'source.fixAll.eslint': true,
+  },
+  'files.exclude': {
+    node_modules: true,
+  },
+  'workbench.settings.editor': 'json',
+  teste: true,
+};
+
+(() => {
+  let settings = {};
+  let hasExistFile = existsSync(_PATH_FOLDER) ?? false;
+  //check has file settings.json inside .vscode folder
+  if (existsSync(_PATH_FOLDER)) settings = require(_PATH_FOLDER);
+  //include non-existing properties in the file
+  for (const key in _DEFAULT_CONFIGS_WORKSPACE) {
+    if (Object.prototype.hasOwnProperty.call(settings, key)) {
+      if (typeof _DEFAULT_CONFIGS_WORKSPACE[key] !== 'string') {
+        for (const keyChild in _DEFAULT_CONFIGS_WORKSPACE[key]) {
+          if (Object.prototype.hasOwnProperty.call(!settings[key], keyChild)) {
+            settings[key][keyChild] = _DEFAULT_CONFIGS_WORKSPACE[key][keyChild];
+          }
+        }
+      }
+    } else {
+      settings[key] = _DEFAULT_CONFIGS_WORKSPACE[key];
+    }
+  }
+  try {
+    const settingsJSON = JSON.stringify(settings);
+    //checks if the .vscode folder created in the root of the project already exists.
+    if (!hasExistFile) {
+      if (!existsSync(_PATH_FOLDER.replace('/settings.json', ''))) {
+        mkdirSync(_PATH_FOLDER.replace('/settings.json', ''));
+      }
+    }
+    writeFileSync(_PATH_FOLDER, settingsJSON);
+    console.log('Arquivo settings.json criado com sucesso.');
+  } catch (err) {
+    console.error('Erro ao criar o arquivo settings.json:', err);
+  }
+})();
