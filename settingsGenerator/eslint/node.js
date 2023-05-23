0a49f2c109e099f7612187a98629c55340c83dfa
Caio Queiroz <caio.cezar.dequeiroz@gmail.com>
2023-05-23 12:14:53 -0300

    refactor: change local to import file

---
+++ settingsGenerator/eslint/node.js
@@ -0,0 +1,56 @@
+module.exports = {
+  root: true,
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:jest/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 80,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: 'all',
+        arrowParens: 'always',
+        semi: true,
+      },
+    ],
+    'no-var': 'error',
+    'import/no-named-export': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+      },
+    ],
+  },
+  settings: {
+    'import/parsers': {
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
+    },
+    'import/resolver': {
+      typescript: {},
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+};
---
+++ settingsGenerator/prettier/index.js
@@ -0,0 +1,73 @@
+/* eslint-disable no-console */
+const { existsSync, writeFileSync, readFileSync } = require('node:fs');
+const yaml = require('js-yaml');
+const _PATH_FOLDER = `${process.cwd()}/.prettierrc`;
+const fileExtensions = ['.yml', '.yaml', '.js', '.json'];
+const _DEFAULT_CONFIGS_PRETTIER = {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  semi: true,
+  teste: true,
+};
+
+(() => {
+  let settings = {};
+  let fileExtension = 99;
+  //checks if the file exists and what the .prettierrc file extension
+  const configFileExists = fileExtensions.some((extension, index) => {
+    fileExtension = fileExtensions[index];
+    return existsSync(`${_PATH_FOLDER}${extension}`);
+  });
+
+  fileExtension = !configFileExists ? 99 : fileExtension;
+
+  //If the .prettierrc file has the .yml or .yaml extension, the yml is converted to js
+  if (
+    configFileExists &&
+    (fileExtension === fileExtensions[0] || fileExtension === fileExtensions[1])
+  ) {
+    const fileContent = readFileSync(`${_PATH_FOLDER}${fileExtension}`, 'utf8');
+    settings = yaml.load(fileContent);
+  }
+  //If the .prettierrc file has a .js or .json extension, the values are captured
+  if (
+    configFileExists &&
+    fileExtension !== fileExtensions[0] &&
+    fileExtension !== fileExtensions[1]
+  ) {
+    settings = require(`${_PATH_FOLDER}${fileExtension}`);
+  }
+
+  //include non-existing properties in the file
+  for (const key in _DEFAULT_CONFIGS_PRETTIER) {
+    if (!Object.prototype.hasOwnProperty.call(settings, key))
+      settings[key] = _DEFAULT_CONFIGS_PRETTIER[key];
+  }
+
+  try {
+    //Convert settings to yml extension
+    if (
+      configFileExists &&
+      (fileExtension === fileExtensions[0] ||
+        fileExtension === fileExtensions[1])
+    ) {
+      const prettierYAML = yaml.dump(settings);
+      writeFileSync(`${_PATH_FOLDER}${fileExtension}`, prettierYAML);
+    } else {
+      const prettierConfigs = JSON.stringify(settings, null, 2);
+      //Convert settings to JSON or JS and create the file
+      if (fileExtension === fileExtensions[3]) {
+        writeFileSync(`${_PATH_FOLDER}${fileExtension}`, prettierConfigs);
+      } else {
+        const prettierJS = `module.exports = ${prettierConfigs}`;
+        writeFileSync(`${_PATH_FOLDER}.js`, prettierJS);
+      }
+    }
+    console.log('Arquivo .prettier criado com sucesso.');
+  } catch (err) {
+    console.error('Erro ao criar o arquivo .prettier:', err);
+  }
+})();
